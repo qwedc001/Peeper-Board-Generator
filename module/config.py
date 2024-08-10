8501d1ff22103040153536c8c414abddacd7f046
Eric Guo <2364319479@qq.com>
2024-08-10 11:32:26 +0800

    [qwedc001] [WIP] config 改版 加入多OJ支持，大段config代码待覆写

--- module/config.py
+++ module/config.py
@@ -2,18 +2,30 @@ import json
 import os
 
 
-class Config:
-    def __init__(self, work_dir: str, config_path: str = 'config.json'):
+class Configs:
+    def __init__(self, work_dir: str, configs_path: str = 'config.json'):
         self.work_dir = work_dir
-        json_path = os.path.join(self.work_dir, config_path)
+        json_path = os.path.join(self.work_dir, configs_path)
         config_file = open(json_path, 'r', encoding='utf-8')
-        self.jsonCfg = json.loads(config_file.read())
+        self.json_configs = json.loads(config_file.read())
+        self.configs = []
+        for config in self.json_configs:
+            self.configs.append(Config(self.work_dir, config))
+
+
+class Config:
+    def __init__(self, work_dir: str, json_cfg: dict):
+        self.work_dir = work_dir
+        self.jsonCfg = json_cfg
         if not self.jsonCfg['url'].endswith('/'):
             self.jsonCfg['url'] = self.jsonCfg['url'] + '/'
-        config_file.close()
 
     def get_config(self):
         return self.jsonCfg
 
     def set_config(self, key, value):
         self.jsonCfg[key] = value
+
+    @classmethod
+    def from_json(cls, config):
+        pass
